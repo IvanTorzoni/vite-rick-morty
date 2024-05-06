d04de444187fe560d8cc7ca4c210a49c347601f5
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-06 13:08:16 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <h1>CIAO</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+    background-color: black;
+}
+</style>
