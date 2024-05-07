c68b635d1e54973874ad4dd96021bcc5d552c323
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-07 22:16:58 +0200

    feat: added visualization of characters based on their status via selector

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
+import { store } from "./store";
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import CardsList from "./components/CardsList.vue";
+import AppSearch from "./components/AppSearch.vue";
 
 export default {
   components: {
     AppHeader,
     CardsList,
+    AppSearch
   },
   data() {
     return {
+      store,
       cardsArray: [],
     };
   },
@@ -21,24 +25,45 @@ export default {
         this.cardsArray = resp.data.results;
       });
   },
+  methods: {
+    getStatus() {
+      console.log("Get Status", this.store.selectedStatus);
+
+      if (this.store.selectedStatus !== "All") {
+        axios
+        .get("https://rickandmortyapi.com/api/character", {
+          params: {
+            status: this.store.selectedStatus
+          }
+        })
+        .then((resp) => {
+          this.cardsArray = resp.data.results;
+        })
+      } else {
+        axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((resp) => {
+          this.cardsArray = resp.data.results;
+        })
+      }
+    }
+  }
 };
 </script>
 
 <template>
   <div class="test">
     <AppHeader />
+    <AppSearch @filter="getStatus" />
     <CardsList :cardsArray="cardsArray" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 .test {
-   background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/2560px-Rick_and_Morty.svg.png"); 
-    background-position: center;
-    background-size: contain;
-    background-repeat: no-repeat;
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/2560px-Rick_and_Morty.svg.png");
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
 }
-
-
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+      options: ["All", "Alive", "Dead", "unknown"],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container py-4">
+    <div class="d-flex justify-content-between">
+      <div>
+        <select @change="$emit('filter')" v-model="store.selectedStatus">
+          <option :value="state" v-for="state in options">
+            {{ state }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
